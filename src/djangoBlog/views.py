8b23ba6da5d993530bf27e8846f0e7f671b718c7
Horea Dan <danhorea2010@gmail.com>
2020-09-16 16:22:48 +0300

    Drafts no longer visible for authorised user

--- src/djangoBlog/views.py
+++ src/djangoBlog/views.py
@@ -7,14 +7,18 @@ from blog.models import BlogPost
 
 def home_page(request):
 
-    qs = BlogPost.objects.all()[:5]
+    qs = BlogPost.objects.filter(publish_date__isnull=False)[:5]
+
+    if request.user.is_authenticated:
+        my_qs = BlogPost.objects.filter(user=request.user)
+        qs = (qs | my_qs).distinct()
 
     context = {
         "title" : "Welcome",
         "blog_list" : qs,
     }
 
-
+  
     return render(request, "home.html", context)
 
 def about_page(request):
